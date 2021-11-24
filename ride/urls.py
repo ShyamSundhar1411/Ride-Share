f6c840561eddf3293aab4444be6e44eadce670b8
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-11-25 02:30:06 +0530

    Reverting Back

--- ride/urls.py
+++ ride/urls.py
@@ -3,7 +3,12 @@ from django.urls import path,include
 
 
 urlpatterns = [
+    #HostARide
     path('create/',views.HostRide.as_view(),name = "create_ride"),
+    path('ride/<int:pk>/edit/',views.HostRideEditView.as_view(),name ="edit_hosted_ride"),
+    path('delete/created/<int:pk>/ride',views.deleteride,name = "delete_hosted_ride"),
+    #PoolARide
     path('accept/<int:pk>/',views.acceptride,name = "accept_ride"),
     path('cancel/accepted/ride/<int:pk>/',views.cancelride,name = "cancel_ride"),
+
 ]
--- ride/views.py
+++ ride/views.py
@@ -1,9 +1,9 @@
 from . models import RideHost,RidePool
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from django.contrib import messages
 from django.views import generic
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy,reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
@@ -17,19 +17,25 @@ class HostRide(LoginRequiredMixin,generic.CreateView):
     def form_valid(self,form):
         form.instance.user = self.request.user
         return super(HostRide, self).form_valid(form)
+class HostRideEditView(LoginRequiredMixin,generic.UpdateView):
+    model  = RideHost
+    fields = ['start_point','destination','contact']
+    template_name = "ride/HostRideEdit.html"
+    def get_object(self):
+        ride = super(HostRideEditView,self).get_object()
+        if ride.user != self.request.user:
+            raise Http404
+        return ride
+    def get_success_url(self):
+        pk = self.kwargs["pk"]
+        messages.success(self.request,'Updated Successfully')
+        return reverse("edit_hosted_ride", kwargs={"pk": pk})
+
 #Function Based
 @login_required
 def home(request):
     rides = RideHost.objects.all()
-    try:
-        accepted_ride = RidePool.objects.get(user = request.user)
-    except:
-        accepted_ride = None
-    if accepted_ride:
-        a = True
-    else:
-        a = False
-    return render(request,'ride/home.html',{'rides':rides,'isriding':a,"accepted_ride":accepted_ride})
+    return render(request,'ride/home.html',{'rides':rides})
 @login_required
 def acceptride(request,pk):
     accepted_ride = get_object_or_404(RideHost,id = pk)
@@ -52,4 +58,14 @@ def cancelride(request,pk):
         messages.success(request,"Successfully cancelled your ride")
         return redirect("home")
     else:
+        return redirect("home")
+@login_required
+def deleteride(request,pk):
+    accepted_ride = get_object_or_404(RideHost,id = pk)
+    if request.method == "POST":
+        accepted_ride.delete()
+        messages.success(request,"Deleted Ride Successfully")
+        return redirect("home")
+    else:
+        messages.error(request,"Error while processing request")
         return redirect("home")
