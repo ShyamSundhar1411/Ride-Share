d25215929e0333096abec11f6ad4dfb31308da54
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-11-28 21:20:59 +0530

    Re-Deploy

--- rideshare/settings.py
+++ rideshare/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'rideshare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['rideshare/templates','ride/templates'],
+        'DIRS': [os.path.join(BASE_DIR,'rideshare/templates'),os.path.join(BASE_DIR,'ride/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
