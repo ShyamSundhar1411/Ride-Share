e7c7d79ecb8c31afc376460ee01138c89df86c3f
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-11-29 15:34:39 +0530

    Re-Deploy

--- ride/models.py
+++ ride/models.py
@@ -19,8 +19,8 @@ class RideHost(models.Model):
     creation_time = models.DateTimeField(auto_now_add = True)
     start_time = models.TimeField(editable = True)
     status = models.CharField(max_length = 500,choices = Host_Status_Choices,default = "EXPIRED")
-    seats = models.PositiveIntegerField(default = 1,validators = [MaxValueValidator(10),MinValueValidator(0)])
-    available = models.PositiveIntegerField(default = 1,validators = [MaxValueValidator(10),MinValueValidator(0)])
+    seats = models.PositiveIntegerField(default = 1,validators = [MaxValueValidator(10),MinValueValidator(1)])
+    available = models.PositiveIntegerField(default = 1,validators = [MaxValueValidator(10),MinValueValidator(1)])
     slug = models.SlugField(blank = True)
     def __str__(self):
         return str(self.contact)+'-'+str(self.destination)
