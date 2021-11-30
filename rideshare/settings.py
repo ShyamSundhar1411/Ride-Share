b8944bbcb161a435a34d271e39d759d13a5ff04c
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-11-30 18:14:34 +0530

    Date-Picker

--- rideshare/settings.py
+++ rideshare/settings.py
@@ -176,7 +176,7 @@ DEFAULT_FROM_EMAIL = "VITrendz Chennai Tech Team <noreply@rideshare.com>"
 
 #Celery Settings
 CELERY_BROKER_URL = os.environ["REDIS_URL"]
-CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
